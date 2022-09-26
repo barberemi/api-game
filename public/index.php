bfe24fea6d1d061595b0b0d38f302a5818473a5f
Barbé <barbe.remi25@gmail.com>
2022-09-27 00:55:53 +0200

    remove restict domain name

--- public/index.php
+++ public/index.php
@@ -12,18 +12,16 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-if (strpos($_SERVER['HTTP_HOST'], 'localhost') === 0 || $_SERVER['REMOTE_HOST'] === 'masters-guild.com') {
-    if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
-        Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
-    }
-
-    if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
-        Request::setTrustedHosts([$trustedHosts]);
-    }
-
-    $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-    $request = Request::createFromGlobals();
-    $response = $kernel->handle($request);
-    $response->send();
-    $kernel->terminate($request, $response);
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
 }
+
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts([$trustedHosts]);
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
