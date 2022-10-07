c031cf0dcfd1dc5b03a543b9e083fc5a13dfb6e8
Barbé <barbe.remi25@gmail.com>
2022-10-08 00:18:05 +0200

    add rewards season on daily attack

--- src/Command/AttackCommand.php
+++ src/Command/AttackCommand.php
@@ -3,7 +3,9 @@
 namespace App\Command;
 
 use App\Entity\Guild;
+use App\Entity\Season;
 use App\Entity\User;
+use App\Entity\OwnItem;
 use App\Helper\AttackHelper;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
@@ -64,6 +66,7 @@ class AttackCommand extends Command
 
         $guilds = $this->entityManager->getRepository(Guild::class)->findAll();
         $count = 0;
+        $currentSeason = null;
 
         /** @var Guild $guild */
         foreach ($guilds as $guild) {
@@ -109,25 +112,139 @@ class AttackCommand extends Command
                 }
 
                 $guild->setDownDraw($downDraw);
-                $guild->setUpDraw($nbScout - $downDraw -1);
+                $guild->setUpDraw($nbScout - $downDraw - 1);
             }
 
             /** **************** **/
             /** 2 -  ATTACK  - 2 **/
             /** **************** **/
-
             $this->logger->info(sprintf('Guild %s attacked by %d monsters.', $guild->getName(), $guild->getLastTrueAttack()));
 
             if ($guild->getDefense() < $guild->getLastTrueAttack()) {
-
+                // Reset le nombre de jour dans la saison
+                $guild->setSeasonDay(0);
+                // Reset de l'exploration et de la position de guilde
+                $guild->setExploration(null);
+                $guild->setPosition(0);
+                // Reset du boss de guild et des fights en cours
+                $guild->setMonster(null);
+            } else {
+                // Ajout de 1 au compteur de jour de saison
+                $guild->setSeasonDay($guild->getSeasonDay() + 1);
+            }
+            // Check si on a dépassé le record de saison
+            if ($guild->getSeasonDay() > $guild->getSeasonRecord()) {
+                $guild->setSeasonRecord($guild->getSeasonDay());
             }
 
+            // Flush
             if ($count % 30 === 0) {
                 $this->entityManager->flush();
             }
         }
         $this->entityManager->flush();
         $this->logger->info($count . ' guilds attacked successfully.');
+
+        /** **************** **/
+        /** 3 -  SEASON  - 3 **/
+        /** **************** **/
+        date_default_timezone_set('Europe/Paris');
+        $date = new \DateTime();
+        $date->modify("-1 day");
+        $seasons = $this->entityManager->getRepository(Season::class)->findAll();
+
+        /** @var Season $season */
+        foreach ($seasons as $season) {
+            if (
+                $season->getStartingAt()->format("Y-m-d") < $date->format("Y-m-d") &&
+                $season->getEndingAt()->format("Y-m-d") >= $date->format("Y-m-d")
+            ) {
+                $currentSeason = $season;
+            }
+        }
+
+        if (isset($currentSeason)) {
+            // Check si jour de fin de saison
+            if (
+                $currentSeason->getEndingAt()->format("Y-m-d") === $date->format("Y-m-d") &&
+                !$currentSeason->isRewarded()
+            ) {
+                $currentSeason->setIsRewarded(true);
+                $this->logger->info(sprintf('Fin de la saison %d.', $currentSeason->getId()));
+
+                if ($guilds) {
+                    // Order guilds by ranking : season_record desc
+                    usort($guilds, function ($guild1, $guild2) {
+                        return $guild2->getSeasonRecord() <=> $guild1->getSeasonRecord();
+                    });
+
+                    // Check des objets à donner aux joueurs du TOP 1 des guildes
+                    /** @var OwnItem $ownItem */
+                    foreach ($currentSeason->getItemsRewarded1() as $ownItem) {
+                        /** @var User $user */
+                        foreach ($guilds[0]->getUsers() as $user) {
+                            $newOwnItem = (new OwnItem())->setUser($user)->setItem($ownItem->getItem());
+                            $user->addItem($newOwnItem);
+                        }
+                        $this->logger->info(sprintf('Obtention objet %s pour la guilde %s.', $ownItem->getItem()->getName(), $guilds[0]->getName()));
+                    }
+
+                    // Check des objets à donner aux joueurs du TOP 2-9 des guildes
+                    /** @var OwnItem $ownItem */
+                    foreach ($currentSeason->getItemsRewarded2() as $ownItem) {
+                        for ($i = 1; $i <= 9; $i++) {
+                            // Pas de guilde on stop
+                            if (!array_key_exists($i, $guilds)) {
+                                break;
+                            }
+                            /** @var User $user */
+                            foreach ($guilds[$i]->getUsers() as $user) {
+                                $newOwnItem = (new OwnItem())->setUser($user)->setItem($ownItem->getItem());
+                                $user->addItem($newOwnItem);
+                            }
+                            $this->logger->info(sprintf('Obtention objet %s pour la guilde %s.', $ownItem->getItem()->getName(), $guilds[$i]->getName()));
+                        }
+                    }
+
+                    // Check des objets à donner aux joueurs du TOP 10-50 des guildes
+                    /** @var OwnItem $ownItem */
+                    foreach ($currentSeason->getItemsRewarded3() as $ownItem) {
+                        for ($i = 10; $i <= 50; $i++) {
+                            // Pas de guilde on stop
+                            if (!array_key_exists($i, $guilds)) {
+                                break;
+                            }
+                            /** @var User $user */
+                            foreach ($guilds[$i]->getUsers() as $user) {
+                                $newOwnItem = (new OwnItem())->setUser($user)->setItem($ownItem->getItem());
+                                $user->addItem($newOwnItem);
+                            }
+                            $this->logger->info(sprintf('Obtention objet %s pour la guilde %s.', $ownItem->getItem()->getName(), $guilds[$i]->getName()));
+                        }
+                    }
+
+                    // Check des objets à donner aux joueurs du TOP 51-* des guildes
+                    /** @var OwnItem $ownItem */
+                    foreach ($currentSeason->getItemsRewarded4() as $ownItem) {
+                        for ($i = 51; $i <= 1000; $i++) {
+                            // Pas de guilde on stop
+                            if (!array_key_exists($i, $guilds)) {
+                                break;
+                            }
+                            /** @var User $user */
+                            foreach ($guilds[$i]->getUsers() as $user) {
+                                $newOwnItem = (new OwnItem())->setUser($user)->setItem($ownItem->getItem());
+                                $user->addItem($newOwnItem);
+                            }
+                            $this->logger->info(sprintf('Obtention objet %s pour la guilde %s.', $ownItem->getItem()->getName(), $guilds[$i]->getName()));
+                        }
+                    }
+                    $this->logger->info(sprintf('Objets de fin de saison données avec succès.'));
+                }
+            }
+        }
+
+        $this->entityManager->flush();
 
         $this->logger->info(sprintf('[Reset Attack] Finished.'));
 
